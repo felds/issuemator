a52f605a3fc5e47868f2bfebdb1c030a5629623d
Felds Liscia <dev@felds.com.br>
2017-12-25 00:39:36 -0200

    install a bunch of stuff because of poor documentation

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,11 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
 ];
